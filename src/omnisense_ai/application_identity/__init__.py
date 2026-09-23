c4c8229f5fa941294b0308baaa18fefb1ba11947
Ratandeep-purohit <rajatpurohit183@outlook.com>
2026-09-23 11:52:23 +0530

    feat: build production engineering foundations

---
+++ src/omnisense_ai/application_identity/__init__.py
@@ -0,0 +1,4 @@
+"""Canonical Windows application identity contracts."""
+from .models import ApplicationIdentity
+from .service import ApplicationIdentityService
+__all__ = ["ApplicationIdentity", "ApplicationIdentityService"]
